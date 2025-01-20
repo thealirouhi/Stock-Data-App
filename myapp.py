deff102c2837086ec56ea5e5c5a6f090d2f7e3a8
Ali Rouhi <thealirouhi@gmail.com>
2025-01-20 14:00:46 +0330

    adding some options for ticker symbol

--- myapp.py
+++ myapp.py
@@ -10,7 +10,12 @@ st.write("""
 """
 )
 
-tickerSymbol = st.text_input("Ticker Symbol", "GOOGL")
+tickerSymbol = st.selectbox(
+    "Ticker Symbol",
+    ("GOOGL", "AAPL", "ADBE","ABNB","AMZN"),
+    index=0,
+    placeholder="Select ticker symbol...",
+)
 
 startDate = st.date_input("Start date", datetime.date(2010, 1, 1))
 
@@ -19,12 +24,12 @@ endDate = st.date_input("End date", datetime.date(2020, 1, 1),max_value="today")
 tickerData = yf.Ticker(tickerSymbol)
 
 tickerDF = tickerData.history(period="1d", start=startDate,end=endDate)
-
+st.write("""tickerSymbol""",tickerSymbol
+    )
 st.write("""
     ## Closing price
 """
 )
-
 st.line_chart(tickerDF.Close)
 
 st.write("""
