c07bb43b5255014c30e00c9e69abc155ee102a45
Ali Rouhi <thealirouhi@gmail.com>
2025-01-20 13:42:03 +0330

    choosing start, end and ticker symbol

--- myapp.py
+++ myapp.py
@@ -1,17 +1,35 @@
 import yfinance as yf
 import streamlit as st
+import datetime
 
+tickerSymbol = "GOOGL"
 st.write("""
-    # hello world!
+    # Market Data – Price & Volume
+         
+
 """
 )
 
 tickerSymbol = st.text_input("Ticker Symbol", "GOOGL")
 
+startDate = st.date_input("Start date", datetime.date(2010, 1, 1))
+
+endDate = st.date_input("End date", datetime.date(2020, 1, 1),max_value="today")
+
 tickerData = yf.Ticker(tickerSymbol)
 
-tickerDF = tickerData.history(period="1d", start='2010-01-01',end='2020-01-01')
+tickerDF = tickerData.history(period="1d", start=startDate,end=endDate)
+
+st.write("""
+    ## Closing price
+"""
+)
 
 st.line_chart(tickerDF.Close)
+
+st.write("""
+    ## Volume
+"""
+)
 
 st.line_chart(tickerDF.Volume)
