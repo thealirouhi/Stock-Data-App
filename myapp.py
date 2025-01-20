4410e95e45401f0249c7e49b5f69dbe82ddbdf72
Ali Rouhi <thealirouhi@gmail.com>
2025-01-20 13:04:53 +0330

    gathering data from yfinance

--- myapp.py
+++ myapp.py
@@ -5,3 +5,9 @@ st.write("""
     # hello world!
 """
 )
+
+tickerSymbol = st.text_input("Ticker Symbol", "GOOGL")
+
+tickerData = yf.Ticker(tickerSymbol)
+
+tickerDF = tickerData.history(period="1d", start='2010-01-01',end='2020-01-01')
